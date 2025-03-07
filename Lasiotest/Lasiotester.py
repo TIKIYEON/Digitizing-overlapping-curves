a42c87405f5096bffd9685170b9a1a61baa9c44b
William <lmf388@alumni.dk>
2025-03-07 11:06:38 +0100

    folders added with lasio, opencv, and colourstampremover

---
+++ Lasiotest/Lasiotester.py
@@ -0,0 +1,34 @@
+#Lasiotester
+import matplotlib.pyplot as plt
+import numpy as np
+lasfn = "../T14502Las/T14502_02-Feb-07_JewelryLog.las"
+import lasio
+las = lasio.read(str(lasfn),ignore_header_errors=True)
+#las = lasio.read(str(lasfn),encoding="cp866")
+#las = lasio.read(str(lasfn),encoding="windows-1251")
+
+headers=las.keys()
+units = {}
+for j in range(0, len(headers)):
+     uval = las.curves[j].unit
+     units[headers[j]] = uval
+
+dataarr = las.data
+metaheaders=las.well.keys()
+metadata=[]
+metadata.append({})
+metadata.append({})
+metadata.append({})
+
+for j in range(0, len(metaheaders)):
+     uval = las.well[j].unit
+     metadata[0][metaheaders[j]] = uval
+
+     tval = las.well[j].value
+     metadata[1][metaheaders[j]] = str(tval)
+
+     dval = las.well[j].descr
+     metadata[2][metaheaders[j]] = str(dval)
+
+print(dataarr)
+
